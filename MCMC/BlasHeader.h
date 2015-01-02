164cfcb97198035dafc6838de97258e52509dbdf
Aaron Meyer <aminute@gmail.com>
2015-01-02 10:11:40 -0500

    Cleanup of old code.

--- MCMC/BlasHeader.h
+++
@@ -1,5 +0,0 @@
-
-int calcProfileMatlab(double *, double *, double *, int, double, int);
-int matlabDiffTPS(double *, double *, int, double *, double *, int, double *, double, double);
-int matlabDiffTPS_pY(double *, double *, int, double *, double *, int, double *, double, double, int);
-int matlabDiffTPS_pYavg(double *, double *, int, double *, double *, int, double *, double, double, int);
